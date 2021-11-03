7f2607679b92af18e1e0a903fd34185591652633
ldh7228 <ldh7228@gmail.com>
2021-11-03 13:16:38 +0900

    [DESIGN] search

--- src/components/search/styles.js
+++ src/components/search/styles.js
@@ -1,29 +1,27 @@
 import styled from "styled-components";
 
 export const MainDiv = styled.div`
-    display: flex;
-    justify-content: center;
-    
-    margin-top: 10%;
-    & input {
-        width: 40%;
-        border: #A8A8A8 2px solid;
-        padding: 15px;
-        font-size: 15px;
-        border-radius: 4px;
-        margin-right: 10px;
-        font-weight: bold;
-    }
-    & input::placeholder {
-        color:  #A8A8A8;
-    }
-    & button {
-        background-color: #0090FF;
-        color: white;
-        border: none;
-        padding: 15px;
-        font-size: 15px;
-        border-radius: 4px;
-        font-weight: bold;
-    }
-`
+  padding: 0% 20%;
+  margin-top: 7%;
+  & input {
+    width: 80%;
+    border: #c4c4c4 2px solid;
+    padding: 15px;
+    font-size: 15px;
+    border-radius: 4px;
+    margin-right: 10px;
+    font-weight: bold;
+  }
+  & input::placeholder {
+    color: #afafaf;
+  }
+  & button {
+    background-color: #0090ff;
+    color: white;
+    border: none;
+    padding: 15px;
+    font-size: 15px;
+    border-radius: 4px;
+    font-weight: bold;
+  }
+`;
