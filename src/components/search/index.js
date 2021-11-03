67b5a705ce3b195f9c4e580ebd3e5a2d3df84b4e
ldh7228 <ldh7228@gmail.com>
2021-11-03 12:31:33 +0900

    [DESIGN] input

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -1,10 +1,11 @@
 import React from "react";
+import * as S from "./styles"
 
 export default function Search() {
   return (
-    <>
+    <S.MainDiv>
       <input placeholder="책 제목을 입력하세요."/>
       <button>검색</button>
-    </>
+    </S.MainDiv>
   );
 }
---
+++ src/components/search/styles.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const MainDiv = styled.div`
+    display: flex;
+    justify-content: center;
+    
+    margin-top: 10%;
+    input {
+        width: 40%;
+        border: #A8A8A8 2px solid;
+        padding: 15px;
+        font-size: 15px;
+        border-radius: 4px;
+        margin-right: 10px;
+        font-weight: bold;
+    }
+    input::placeholder {
+        color:  #A8A8A8;
+    }
+    button {
+        background-color: #0090FF;
+        color: white;
+        border: none;
+        padding: 15px;
+        font-size: 15px;
+        border-radius: 4px;
+        font-weight: bold;
+    }
+`
