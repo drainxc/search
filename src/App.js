875766a6d8c04060b56a518328032de88c56daf3
ldh7228 <ldh7228@gmail.com>
2021-11-03 11:51:20 +0900

    [ADD] search

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Search from "./components/search";
+
+function App() {
+  return (
+    <>
+      <Search/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/search/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Search() {
+  return (
+    <>
+      <input placeholder="책 제목을 입력하세요."/>
+      <button>검색</button>
+    </>
+  );
+}
