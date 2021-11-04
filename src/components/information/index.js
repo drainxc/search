3fe81437aaeecbc4b2218f6028812a958675fc16
ldh7228 <ldh7228@gmail.com>
2021-11-04 14:45:42 +0900

    [ADD] information
    정보 추가

--- src/components/information/index.js
+++ src/components/information/index.js
@@ -1,29 +1,30 @@
 import React from "react";
 import * as S from "./styles";
-import image from "../../asset/image.png";
 
-export default function Information() {
+export default function Information({ book }) {
   return (
     <S.MainDiv>
       <div className="cover">
-        <img src={image} alt="" />
+        <img src={book.thumbnail} alt="" />
       </div>
       <div className="information">
         <div>
           <div className="detail">제목</div>
-          <div className="contents">아무것도 없는 책</div>
+          <div className="contents">{book.title}</div>
         </div>
         <div>
           <div className="detail">저자</div>
-          <div className="contents">김팔복</div>
+          <div className="contents">{book.authors}</div>
         </div>
         <div>
           <div className="detail">가격</div>
-          <div className="contents">12000원</div>
+          <div className="contents">{book.price}원</div>
         </div>
         <div>
           <div className="detail">설명</div>
-          <div className="contents">ㅐㅓ댜운파ㅓㅜ대ㅑㅍㅇ랴ㅐㅓsakfjalksdjfaklsdjfklajsdklfjaklsdfjkasdjflkasdjflㅁㄴ론어ㅏ로ㅜㅏㅓㄴ오럼농럼노아ㅓㄹ몬아ㅓ로마ㅓㄴ올마ㅣㅘㅓㅁㄴ오리ㅏkadsjflkjsadlkfjaslkdfjlㅡㅐ쟏릉니ㅏㅡㅑㅐㄷㄹ</div>
+          <div className="contents">
+            {book.contents}
+          </div>
         </div>
       </div>
     </S.MainDiv>
