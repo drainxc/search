1f7a57d8e0882002c1701eed78cbcfe9986de153
ldh7228 <ldh7228@gmail.com>
2021-11-04 14:45:09 +0900

    [ADD] book

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -1,11 +1,41 @@
-import React from "react";
-import * as S from "./styles"
+import React, { useState } from "react";
+import * as S from "./styles";
+import { bookSearch } from "../../lib/api";
+import Information from "../information";
 
 export default function Search() {
+  const [book, setBook] = useState([]);
+  const [text, setText] = useState("");
+
+  const onTextChange = (e) => {
+    setText(e.target.value);
+  };
+
+  async function bookSearchHttpHandler() {
+    const params = {
+      query: text,
+      sort: "accuracy",
+      page: 1,
+      size: 1,
+    };
+    const { data } = await bookSearch(params);
+    setBook(data.documents[0]);
+    console.log(book);
+  }
+
   return (
-    <S.MainDiv>
-      <input placeholder="책 제목을 입력하세요."/>
-      <button>검색</button>
-    </S.MainDiv>
+    <>
+      <S.MainDiv>
+        <input
+          type="search"
+          placeholder="책 제목을 입력하세요."
+          name="query"
+          onChange={onTextChange}
+          value={text}
+        />
+        <button onClick={bookSearchHttpHandler}>검색</button>
+      </S.MainDiv>
+      <Information book={book} />
+    </>
   );
 }
