07547d6898af5bc6408c07ea7738941bc2f2b902
ldh7228 <ldh7228@gmail.com>
2021-11-04 14:26:40 +0900

    [ADD] rest api key

--- src/lib/api/index.js
+++ src/lib/api/index.js
@@ -3,10 +3,10 @@ import axios from "axios";
 const Kakao = axios.create({
   baseURL: "https://dapi.kakao.com",
   headers: {
-    Authorization: 'KakaoAK {REST_API_KEY}',
-  },
+    Authorization: "KakaoAK 68151afc527e2fe603000910f1c865d0"
+  }
 });
 
-export const bookSearch = (params) => {
-    return Kakao.get('/v3/search/book', { params })
-}
+export const bookSearch = params => {
+  return Kakao.get("/v3/search/book", { params });
+};
